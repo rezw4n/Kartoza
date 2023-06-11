b54c9622cab7ea7914822abea423e1c50776c496
Rezwan Ahmed <rezwan490@gmail.com>
2023-06-11 20:00:49 +0600

    Added docstring for user model class.

--- Users/models.py
+++ Users/models.py
@@ -4,6 +4,18 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class UserProfile(models.Model):
+    """
+    Represents a user profile with additional information.
+
+    Attributes:
+        user (OneToOneField): The associated user object.
+        email (EmailField): The email address of the user profile.
+        profile_pic (ImageField): The profile picture of the user.
+        home_address (TextField): The home address of the user.
+        phone_number (CharField): The phone number of the user.
+        latitude (FloatField): The latitude coordinate of the user's location.
+        longitude (FloatField): The longitude coordinate of the user's location.
+    """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     email = models.EmailField(max_length=254, blank=True)
     profile_pic = models.ImageField(default='/ProfilePics/default.jpg', upload_to='ProfilePics')
