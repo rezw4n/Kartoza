0c570e09887665a9b4fb359377b034fe74dd9ffd
Rezwan Ahmed <rezwan490@gmail.com>
2023-06-11 11:41:42 +0600

    Changed home address field type in models.

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Home',
     'Users',
 ]
 
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'Portfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [Path.joinpath(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- Users/models.py
+++ Users/models.py
@@ -5,8 +5,9 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    email = models.EmailField(max_length=254, blank=True)
     profile_pic = models.ImageField(default='/ProfilePics/default.jpg', upload_to='ProfilePics')
-    home_address = models.CharField("Home Address", max_length=100)
+    home_address = models.TextField("Home Address")
     phone_number = models.CharField("Phone Number", max_length=20)
     latitude = models.FloatField("Latitude (Y)", validators=[MinValueValidator(-90), MaxValueValidator(90)]) # The latitude is limited within -90 to +90
     longitude = models.FloatField("Longitude (X)", validators=[MinValueValidator(-180), MaxValueValidator(180)]) # The longitude is limited within -180 to +18
@@ -16,4 +17,4 @@ class UserProfile(models.Model):
             return (self.longitude, self.latitude)
     
     def __str__(self):
-        return self.user.username
+        return self.user.first_name + " " + self.user.last_name
